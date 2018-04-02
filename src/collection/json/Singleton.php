859405b34bf14ac1a49ae5b0bf585945c951ef9e
Edwin Rodríguez <edwrodrig@gmail.com>
2018-04-02 12:24:12 -0300

    Changes

--- src/collection/json/Singleton.php
+++ src/collection/json/Singleton.php
@@ -11,15 +11,10 @@ namespace edwrodrig\contento\collection\json;
 
 class Singleton
 {
-    public static function create_from_array(array $elements, string $class) : self {
-        $r = new self;
-        $r->from_array($elements, $class);
-        return $r;
-    }
-
     public static function create_from_json(string $filename, string $class) : self {
-        $elements = json_decode(file_get_contents($filename), true);
+        $element = json_decode(file_get_contents($filename), true);
 
-        return self::create_from_array($elements, $class);
+        return $class::create_from_array($element);
+
     }
 }
